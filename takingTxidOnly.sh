89f0e03587bc110f49377f9ee8fbdd654f7710d5
Risvaldo Christoper Toar <34494943+chronus44@users.noreply.github.com>
2022-06-13 15:14:17 +1000

    Update takingTxidOnly.sh

--- takingTxidOnly.sh
+++ takingTxidOnly.sh
@@ -8,8 +8,11 @@ while IFS=',' read x1 x2
 do
     while IFS=" " read y1 y2 y3 y4 y5 y6 y7 y8 y9
     do
+        # taking only the seventh word from the line
         echo "$y7"
         txid="$y7"
+        
+        # Store the seventh word or the transaction ID into a file
         echo $txid >> transactionsFile.txt
     done < "$file"
 
